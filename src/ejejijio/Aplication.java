6fcdf15b8efbf394a7debf4862f5a694932597d5
DeividZamora <zamoradeivid4@gmail.com>
2024-04-19 14:02:50 -0500

    a

--- src/ejejijio/Aplication.java
+++ src/ejejijio/Aplication.java
@@ -23,8 +23,10 @@ public class Aplication {
 		
 			selec=0;
 			System.out.println("¿Qué acción desea hacer?");
-			System.out.println("1.Consultar saldo. 2.Realizar retiro. 3.Depositar en cuenta. 4. Tramsferir fondos");
-			
+			System.out.println("1.Consultar saldo.");
+			System.out.println("2.Realizar retiro.");
+			System.out.println("3.Depositar en cuenta.");
+			System.out.println("4. Transferir fondos");
 			int opcion = leer.nextInt();
 			
 			switch(opcion) 
@@ -33,7 +35,7 @@ public class Aplication {
 			case 1: banco1.ConsultarSaldo(); break;
 			case 2: System.out.println("Digite la cantidad de dinero que quiere retirar");banco1.Retirar(leer.nextDouble()); break;
 			case 3: System.out.println("Digite la cantidad de dinero que quiere depositar");banco1.Depositar(leer.nextDouble()); break;
-			case 4: a=2;
+			case 4: a=2; break;
 			default: 
 			}
 		}
